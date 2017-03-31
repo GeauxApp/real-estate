7e98ca9bf6644596ed9198328b7abd53de051eaa
laurakathleen <laurakwalley@gmail.com>
2017-03-31 15:57:55 -0700

    adds property images as seed data

--- seed.js
+++ seed.js
@@ -9,21 +9,27 @@ var propertiesList = [
 				city: 'San Francisco',
 				state: 'CA',
 				bedroomCount: 4,
-				bathCount: 3
+				bathCount: 3,
+				showSplashImage: 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-ext.png?raw=true',
+				images: ['https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-ext.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-dining2.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-bed2.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-bed4.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-kitchen2.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-master.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-living.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-patio.png?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/La-Grande/la-grande-backyard.png?raw=true']
 				},
 				{
 				streetAddress: '550 33rd Avenue',
 				city: 'San Francisco',
 				state: 'CA',
 				bedroomCount: 4,
-				bathCount: 4
+				bathCount: 4,
+				showSplashImage: 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-ext.jpg?raw=true',
+				images: ['https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-ext.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-dining.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-den2.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-kitchen.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-living.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-bed.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-bed4.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-bath2.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-bar.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/33rd/33rd-ave-backyard.jpg?raw=true']
 				},
 				{
 				streetAddress: '835 46th Avenue',
 				city: 'San Francisco',
 				state: 'CA',
 				bedroomCount: 3,
-				bathCount: 2	
+				bathCount: 2,
+				showSplashImage: 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-Ave-Ext.jpg?raw=true',
+				images: ['https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-Ave-Ext.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-Den.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-Living.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-bath1.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-bed2.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-kitchen.jpg?raw=true', 'https://github.com/laurakathleen/FW-Construction/blob/master/Images/46th/46th-Backyard.jpg?raw=true'] 
 				}
 			];
 
